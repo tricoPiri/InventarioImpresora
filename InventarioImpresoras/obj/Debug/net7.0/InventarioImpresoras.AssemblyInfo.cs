9b51806fab17f8468b6fe203661ab82a61ec490a
samuel <bcastyyo@gmail.com>
2024-11-26 09:15:08 -0600

    mostrando modal de editar rol en el modulo de roles

--- InventarioImpresoras/obj/Debug/net7.0/InventarioImpresoras.AssemblyInfo.cs
+++ InventarioImpresoras/obj/Debug/net7.0/InventarioImpresoras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventarioImpresoras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf6e9f9d8ae17386d26ed097ef4ba0d82087530")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventarioImpresoras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventarioImpresoras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
