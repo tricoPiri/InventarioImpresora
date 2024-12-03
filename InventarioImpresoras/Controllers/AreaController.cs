e1807055196bf2766517f891643db9ca9dc3a972
Carlos Manuel <carlosm.garciaa20@gmail.com>
2024-12-03 11:38:24 -0600

    consulta de areas en el modulo de areas

---
+++ InventarioImpresoras/Controllers/AreaController.cs
@@ -0,0 +1,33 @@
+﻿using InventarioImpresoras.DAL;
+using InventarioImpresoras.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventarioImpresoras.Controllers
+{
+    public class AreaController : Controller
+    {
+        DAL_Areas objArea = new DAL_Areas();
+        // GET: AreaController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "1")]
+        public JsonResult ConsultarAreas()
+        {
+            List<Areas> listaAreas = new List<Areas>();
+            try
+            {
+                listaAreas = objArea.getAreas();
+            }
+            catch (Exception ex)
+            {
+                DAL_Utilerias.FormatoExcepcion(ex);
+            }
+            return Json(listaAreas);
+        }
+    }
+}
--- InventarioImpresoras/Models/Areas.cs
+++ InventarioImpresoras/Models/Areas.cs
@@ -4,5 +4,6 @@
     {
         public int IdArea { get; set; }
         public string Nombre { get; set; }
+        public bool Activo { get; set; }
     }
 }
