7b0eccfbc4e813366ea03c4b6084ba38cf5ff83b
Carlos Manuel <carlosm.garciaa20@gmail.com>
2024-12-10 11:26:28 -0600

    consulta de impresoras en el modulo de impresoras

--- InventarioImpresoras/Controllers/ImpresoraController.cs
+++ InventarioImpresoras/Controllers/ImpresoraController.cs
@@ -1,4 +1,5 @@
-﻿using InventarioImpresoras.Models;
+﻿using InventarioImpresoras.DAL;
+using InventarioImpresoras.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -7,10 +8,30 @@ namespace InventarioImpresoras.Controllers
 {
     public class ImpresoraController : Controller
     {
+        DAL_Impresoras objImpresora = new DAL_Impresoras();
         [Authorize(Roles = "1")]
         public ActionResult Index()
         {
             return View();
         }
+
+        [Authorize(Roles = "1")]
+        public JsonResult ConsultarImpresoras()
+        {
+            List<Impresoras> listaImpresoras = new List<Impresoras>();
+            try
+            {
+                listaImpresoras = objImpresora.getImpresoras();
+            }
+            catch (Exception ex)
+            {
+                DAL_Utilerias.FormatoExcepcion(ex);
+            }
+            return Json(listaImpresoras);
+        }
     }
 }
+
+
+
+
---
+++ InventarioImpresoras/DAL/DAL_Impresoras.cs
@@ -0,0 +1,72 @@
+﻿using InventarioImpresoras.Models;
+using System.Data.SqlClient;
+using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace InventarioImpresoras.DAL
+{
+    public class DAL_Impresoras
+    {
+        DAL_Conexion objConexion = new DAL_Conexion();
+        public DAL_Impresoras()
+        {
+            objConexion.Conectar();
+        }
+
+        public List<Impresoras> getImpresoras()
+        {
+            List<Impresoras> listaImpresoras = new List<Impresoras>();
+            try
+            {
+                SqlCommand sqlcmd = new SqlCommand("spObtenerImpresoras", objConexion.conexion);
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlcmd);
+                DataTable dataTable = new DataTable();
+                objConexion.conexion.Open();
+                dataAdapter.Fill(dataTable);
+                objConexion.conexion.Close();
+
+                foreach (DataRow dr in dataTable.Rows)
+                {
+                    listaImpresoras.Add(new Impresoras
+                    {
+                        IdImpresora = (int)dr["idImpresora"],
+                        Nombre = (dr["Impresora"].ToString() == "" ? "" : (string)dr["Impresora"]),
+                        NumeroSerie = (dr["numeroSerie"].ToString() == "" ? "" : (string)dr["numeroSerie"]),
+                        Activo = (bool)dr["activo"],
+                        Marcas = new List<Marcas>
+                        {
+                            new Marcas()
+                            {
+                                IdMarca = (int)dr["idMarca"],
+                                Nombre = (dr["Marca"].ToString() == "" ? "" : (string)dr["Marca"]),
+                            }
+                        },
+                        Modelos = new List<Modelos>
+                        {
+                            new Modelos()
+                            {
+                                IdModelo = (int)dr["idModelo"],
+                                Nombre = (dr["Modelo"].ToString() == "" ? "" : (string)dr["Modelo"]),
+                            }
+                        },
+                        Areas = new List<Areas>
+                        {
+                            new Areas()
+                            {
+                                IdArea = (int)dr["idArea"],
+                                Nombre = (dr["Area"].ToString() == "" ? "" : (string)dr["Area"]),
+                            }
+                        },
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                DAL_Utilerias.FormatoExcepcion(ex);
+            }
+            return listaImpresoras;
+        }
+    }
+}
