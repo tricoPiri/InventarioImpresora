7b6c545ed62a9ba2bd712947d943406020c8feb6
santiago <xouldark22@gmail.com>
2024-12-11 13:12:45 -0600

    respaldo de la base de datos con los procedimientos almacenados de lecturas

--- InventarioImpresoras/obj/Debug/net7.0/InventarioImpresoras.AssemblyInfo.cs
+++ InventarioImpresoras/obj/Debug/net7.0/InventarioImpresoras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventarioImpresoras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a065d24a90c063b2ed75dc5e54cee54448b037")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+832e0a7a8948366212bdcb7190efd1601122ee1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventarioImpresoras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventarioImpresoras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
